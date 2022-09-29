577ca17003b55a69d6bd2e850cf16aa351bcc6ec
Ander Cabrera <andercabrerac@gmail.com>
2022-09-28 23:24:09 -0600

    fixing CRAPPP

--- src/org/angello/com/view/pages/Rentar.java
+++ src/org/angello/com/view/pages/Rentar.java
@@ -51,33 +51,33 @@ public class Rentar extends View<Renta, ControllerRenta> {
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 705, Short.MAX_VALUE)
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addGap(0, 0, Short.MAX_VALUE)
-                    .addComponent(jLabel1)
-                    .addGap(0, 0, Short.MAX_VALUE)))
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 705, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(0, 0, Short.MAX_VALUE)
+                                        .addComponent(jLabel1)
+                                        .addGap(0, 0, Short.MAX_VALUE)))
         );
         jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 581, Short.MAX_VALUE)
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addGap(0, 0, Short.MAX_VALUE)
-                    .addComponent(jLabel1)
-                    .addGap(0, 0, Short.MAX_VALUE)))
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 581, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(0, 0, Short.MAX_VALUE)
+                                        .addComponent(jLabel1)
+                                        .addGap(0, 0, Short.MAX_VALUE)))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -85,25 +85,58 @@ public class Rentar extends View<Renta, ControllerRenta> {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    // End of variables declaration//GEN-END:variables
 
     @Override
-    public void render(ArrayList<Renta> models) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void render(ArrayList<Renta> rentas) {
+        setData(rentas);
+        initComponents();
+        myInitComponents();
+        revalidate();
+        repaint();
     }
 
     @Override
-    public ArrayList<ArrayList<String>> modelArrToStringArr2D(ArrayList<Renta> models) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public ArrayList<ArrayList<String>> modelArrToStringArr2D(ArrayList<Renta> rentas) {
+        ArrayList<ArrayList<String>> array = new ArrayList<>();
+
+        for (Renta renta : rentas) {
+            String id = String.valueOf(renta.getIdRenta());
+            String costo = String.valueOf(renta.getCosto());
+            String fechaInicio = String.valueOf(renta.getFechaInicio());
+            String fechaFinal = String.valueOf(renta.getFechaFinal());
+            String placa = String.valueOf(renta.get_vehiculoPlaca());
+            String CUIRentador = String.valueOf(renta.get_clienteCUI());
+            array.add(new ArrayList<String>() {{
+                add(id);
+                add(costo);
+                add(fechaInicio);
+                add(fechaFinal);
+                add(placa);
+                add(CUIRentador);
+            }});
+        }
+        return array;
     }
 
     @Override
     public Renta arrStringToModel(ArrayList<String> tuples) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int id = Integer.parseInt(String.valueOf(table.getValueAt(table.getSelectedRow(), 0)));
+        Double costo = Double.valueOf(tuples.get(1));
+        Date fechaInicio = Date.valueOf(tuples.get(2));
+        Date fechaFinal = Date.valueOf(tuples.get(3));
+        String placa = tuples.get(4);
+        Long CUI = Long.valueOf(tuples.get(5));
+
+        return new Renta(id, costo, fechaInicio, fechaFinal, placa, CUI);
     }
 
     @Override
     public Renta arrStringToModel(ArrayList<String> tuples, boolean isFormFormat) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (!isFormFormat) return arrStringToModel(tuples);
+        Renta renta = arrStringToModel(tuples);
+        renta.setIdRenta(0);
+
+        return renta;
     }
+    // End of variables declaration//GEN-END:variables
 }
